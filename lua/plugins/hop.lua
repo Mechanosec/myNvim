c477c25ee3e282021afbe73d48678a82e816977c
O.Krasovskyi <mechanosec29@gmail.com>
2024-02-01 11:46:15 +0200

    first commit

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,9 @@
+return {
+  "phaazon/hop.nvim",
+  config = function()
+    require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
+  end,
+  keys = {
+    {"<leader><leader>", "<cmd>HopWord<cr>", desc="Hop"}
+  }
+}
