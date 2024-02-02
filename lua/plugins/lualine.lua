6e5d52137ecb52f746d458194a16037e3df4bc39
O.Krasovskyi <mechanosec29@gmail.com>
2024-02-02 11:44:18 +0200

    change lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "dracula",
+				theme = "catppucin",
         globalstatus = true
 				-- disabled_filetypes = { "neo-tree" },
 			},
