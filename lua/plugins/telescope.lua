e823138041eba390da45e3ebb8e14ab2d1bfe90b
O.Krasovskyi <mechanosec29@gmail.com>
2024-02-02 13:28:49 +0200

    fix telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,6 +27,10 @@ return {
 
       require("telescope").setup({
         defaults = {
+          file_ignore_patterns = { "node_modules" },
+          selection_strategy = "reset",
+          sorting_strategy = "ascending",
+          layout_strategy = "horizontal",
           layout_config = {
             horizontal = {
               prompt_position = "top",
