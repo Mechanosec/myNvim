90bcf46c8183d0cc50ab21f7e6378d5eabe67ece
O.Krasovskyi <mechanosec29@gmail.com>
2024-02-02 16:58:04 +0200

    try use comment sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,19 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = { "HiPhish/nvim-ts-rainbow2", "windwp/nvim-ts-autotag" },
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = { "HiPhish/nvim-ts-rainbow2", "windwp/nvim-ts-autotag" },
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			auto_install = true,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-			rainbow = { enable = true },
-			autotag = { enable = true },
-		})
-	end,
+			configs.setup({
+				auto_install = true,
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				rainbow = { enable = true },
+				autotag = { enable = true },
+			})
+		end,
+	},
 }
