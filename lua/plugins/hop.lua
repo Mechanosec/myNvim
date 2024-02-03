5ff81212eafa6e0b2989e09358ec8a02ce0e4655
O.Krasovskyi <mechanosec29@gmail.com>
2024-02-03 22:09:32 +0200

    add gitsings

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,9 +1,9 @@
 return {
-  "phaazon/hop.nvim",
-  config = function()
-    require("hop").setup { keys = "etovxqpdygfblzhckisuran" }
-  end,
-  keys = {
-    {"<leader>m", "<cmd>HopWord<cr>", desc="HopWord"}
-  }
+	"phaazon/hop.nvim",
+	config = function()
+		require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+	end,
+	keys = {
+		{ "<leader>m", "<cmd>HopWord<cr>", desc = "HopWord" },
+	},
 }
