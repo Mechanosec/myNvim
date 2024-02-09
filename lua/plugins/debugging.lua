fe91f47f7a868f4e14c4f4833d7c2ef1a407dc65
O.Krasovskyi <mechanosec29@gmail.com>
2024-02-09 23:32:41 +0200

    setup debug

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,90 +1,57 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-      local dap = require("dap")
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			local dap = require("dap")
 
-      -- Set keymaps to control the debugger
-      vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
-      vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+			vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
+			vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		config = function()
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "node2" },
+				handlers = {
+					function(config)
+						require("mason-nvim-dap").default_setup(config)
+					end,
+				},
+			})
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
 
-      -- JS based languages
+			dapui.setup({
+				layouts = {
+					{
+						elements = {
+							-- Elements can be strings or table with id and size keys.
+							{ id = "scopes", size = 0.7 },
+							{ id = "breakpoints", size = 0.3 },
+						},
+						size = 85,
+						position = "right",
+					},
+				},
+			})
 
-      local DEBUGGER_PATH = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open({})
+			end
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close({})
+			end
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close({})
+			end
 
-      require("dap-vscode-js").setup({
-        debugger_path = DEBUGGER_PATH, -- Path to vscode-js-debug installation.
-        adapters = {
-          "chrome",
-          "pwa-node",
-          "pwa-chrome",
-          "pwa-msedge",
-          "node-terminal",
-          "pwa-extensionHost",
-          "node",
-          "chrome",
-        }, -- which adapters to register in nvim-dap
-      })
-
-      local js_based_languages = { "typescript", "javascript", "typescriptreact" }
-
-      for _, language in ipairs(js_based_languages) do
-        require("dap").configurations[language] = {
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}",
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require("dap.utils").pick_process,
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "pwa-chrome",
-            request = "launch",
-            name = 'Start Chrome with "localhost"',
-            url = "http://localhost:3000",
-            webRoot = "${workspaceFolder}",
-            userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
-          },
-        }
-      end
-    end,
-  },
-  {
-    "mxsdev/nvim-dap-vscode-js",
-    dependencies = { "mfussenegger/nvim-dap" },
-  },
-  {
-    "microsoft/vscode-js-debug",
-    lazy = false,
-    build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-  },
-
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap" },
-    config = function()
-      require("dapui").setup()
-
-      local dap, dapui = require("dap"), require("dapui")
-
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open({})
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close({})
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close({})
-      end
-
-      vim.keymap.set("n", "<leader>dU", dapui.toggle)
-    end,
-  },
+			vim.keymap.set("n", "<leader>dU", dapui.toggle)
+		end,
+	},
 }
