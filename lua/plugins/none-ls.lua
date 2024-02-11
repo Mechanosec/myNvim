4fbd968fb636e1b613d3a7d490856c42a35c6403
O.Krasovskyi <mechanosec29@gmail.com>
2024-02-11 03:05:45 +0200

    fix format

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,29 +1,28 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		event = "VeryLazy",
-		config = function()
-			local null_ls = require("null-ls")
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
 
-			local formatting = null_ls.builtins.formatting
-			local lint = null_ls.builtins.diagnostics
-			local code_actions = null_ls.builtins.code_actions
+      local formatting = null_ls.builtins.formatting
+      local lint = null_ls.builtins.diagnostics
+      local code_actions = null_ls.builtins.code_actions
 
-			null_ls.setup({
-				sources = {
-					-- Web
-					formatting.prettier,
-					lint.eslint_d.with({
-						method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-					}),
-					code_actions.eslint_d,
-					-- Lua
-					formatting.stylua,
+      null_ls.setup({
+        sources = {
+          -- Web
+          formatting.prettier,
+          lint.eslint_d.with({
+            method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+          }),
+          code_actions.eslint_d,
+          -- Lua
+          formatting.stylua,
 
-					-- yaml
-					formatting.yamlfmt,
-				},
-			})
-		end,
-	},
+          -- yaml
+          formatting.yamlfmt,
+        },
+      })
+    end,
+  },
 }
