aaa6e22f65964681a7b8a05e8c788aa06e8843da
O.Krasovskyi <mechanosec29@gmail.com>
2024-02-11 03:10:21 +0200

    check exist eslint file

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,28 +1,31 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
+	{
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
 
-      local formatting = null_ls.builtins.formatting
-      local lint = null_ls.builtins.diagnostics
-      local code_actions = null_ls.builtins.code_actions
+			local formatting = null_ls.builtins.formatting
+			local lint = null_ls.builtins.diagnostics
+			local code_actions = null_ls.builtins.code_actions
 
-      null_ls.setup({
-        sources = {
-          -- Web
-          formatting.prettier,
-          lint.eslint_d.with({
-            method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-          }),
-          code_actions.eslint_d,
-          -- Lua
-          formatting.stylua,
+			null_ls.setup({
+				sources = {
+					-- Web
+					formatting.prettier,
+					lint.eslint_d.with({
+						method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+						condition = function(utils)
+							return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
+						end,
+					}),
+					code_actions.eslint_d,
+					-- Lua
+					formatting.stylua,
 
-          -- yaml
-          formatting.yamlfmt,
-        },
-      })
-    end,
-  },
+					-- yaml
+					formatting.yamlfmt,
+				},
+			})
+		end,
+	},
 }
