8eea840fb2ef85f8bf36e84f8e10833ac15d7fdf
O.Krasovskyi <mechanosec29@gmail.com>
2024-02-15 13:19:53 +0200

    add line diagnostic

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,17 @@
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons", "arkav/lualine-lsp-progress" },
   config = function()
     require("lualine").setup({
       options = {
         globalstatus = true,
         -- disabled_filetypes = { "neo-tree" },
+      },
+      sections = {
+        lualine_c = {
+          "lsp_progress",
+        },
       },
     })
   end,
