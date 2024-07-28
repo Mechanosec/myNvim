3f7704668cc03a09553fe49486a7496531f2cb8c
O.Krasovskyi <mechanosec29@gmail.com>
2024-07-28 11:58:45 +0300

    add plugins for git

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -8,7 +8,23 @@ return {
 			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 		},
 	},
+	{ "akinsho/git-conflict.nvim", version = "*", config = true },
 	{
 		"sindrets/diffview.nvim",
+		keys = {
+			{ "<leader>gdd", ":DiffviewOpen<cr>", desc = "Diffview open" },
+			{ "<leader>gdc", ":DiffviewClose<cr>", desc = "Diffview close" },
+			{ "<leader>gdh", ":DiffviewFileHistory %<cr>", desc = "Current file history" },
+			{ "<leader>gdH", ":DiffviewFileHistory<cr>", desc = "All files history" },
+		},
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 700,
+			},
+		},
 	},
 }
