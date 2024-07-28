c450717f75c5e850f3339313ea0dd59a36599e3d
O.Krasovskyi <mechanosec29@gmail.com>
2024-07-28 05:11:34 +0300

    add dap

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,38 +1,73 @@
 return {
 	{
-  'stevearc/dressing.nvim',
-  opts = {},
-},
-{
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {},
-  keys = {
-     {
-      "<leader>f",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "+Find",
-    },
-  },
-},
-{
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-		require("lualine").setup({
-			options = {
-				globalstatus = true,
-				-- disabled_filetypes = { "neo-tree" },
+		"stevearc/dressing.nvim",
+		opts = {},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "+Find",
 			},
-			sections = {
-				lualine_c = {
-					"filename",
-					"lsp_progress",
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					globalstatus = true,
+					-- disabled_filetypes = { "neo-tree" },
+				},
+				sections = {
+					lualine_c = {
+						"filename",
+						"lsp_progress",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		lazy = false,
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		keys = {
+			{ "<Tab>", ":BufferLineCycleNext<cr>" },
+			{ "<S-Tab>", ":BufferLineCyclePrev<cr>" },
+		},
+		opts = {},
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 				},
 			},
-		})
-	end,
-}
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
+			},
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+	},
 }
