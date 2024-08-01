48cbb9d74065188d521f34b0322ce7eedcefb7a6
O.Krasovskyi <mechanosec29@gmail.com>
2024-08-01 20:17:23 +0300

    change notify style

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -92,7 +92,7 @@ return {
 		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
-			{ "rcarriga/nvim-notify", opts = { timeout = 1 } },
+			{ "rcarriga/nvim-notify", opts = { timeout = 500, render = "wrapped-compact" } },
 		},
 	},
 	{
