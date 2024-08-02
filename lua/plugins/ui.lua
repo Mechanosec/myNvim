1e8a21741bb264697e4504f32ff77a2e04c6c447
O.Krasovskyi <mechanosec29@gmail.com>
2024-08-02 10:55:56 +0300

    add ui plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -72,6 +72,32 @@ return {
 		opts = {},
 	},
 	{
+		"b0o/incline.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("incline").setup({
+				highlight = {
+					groups = {
+						InclineNormal = { guibg = "#44406e" },
+					},
+				},
+				window = { margin = { vertical = 0, horizontal = 1 } },
+				hide = {
+					cursorline = true,
+				},
+				render = function(props)
+					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+					if vim.bo[props.buf].modified then
+						filename = "[+] " .. filename
+					end
+
+					local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+					return { { icon, guifg = color }, { " " }, { filename } }
+				end,
+			})
+		end,
+	},
+	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		opts = {
