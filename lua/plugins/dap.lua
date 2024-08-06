4eb3dd98ff30294dc2e40602bd9f30f860b0a7ae
O.Krasovskyi <mechanosec29@gmail.com>
2024-08-06 16:18:30 +0300

    update keys for dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,13 +3,6 @@ return {
 		"mfussenegger/nvim-dap",
 		keys = {
 			{
-				"<leader>dB",
-				function()
-					require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-				end,
-				desc = "Breakpoint Condition",
-			},
-			{
 				"<leader>db",
 				function()
 					require("dap").toggle_breakpoint()
@@ -24,53 +17,11 @@ return {
 				desc = "Continue",
 			},
 			{
-				"<leader>da",
-				function()
-					require("dap").continue({ before = get_args })
-				end,
-				desc = "Run with Args",
-			},
-			{
-				"<leader>dC",
-				function()
-					require("dap").run_to_cursor()
-				end,
-				desc = "Run to Cursor",
-			},
-			{
-				"<leader>dg",
-				function()
-					require("dap").goto_()
-				end,
-				desc = "Go to Line (No Execute)",
-			},
-			{
 				"<leader>di",
 				function()
 					require("dap").step_into()
 				end,
 				desc = "Step Into",
-			},
-			{
-				"<leader>dj",
-				function()
-					require("dap").down()
-				end,
-				desc = "Down",
-			},
-			{
-				"<leader>dk",
-				function()
-					require("dap").up()
-				end,
-				desc = "Up",
-			},
-			{
-				"<leader>dl",
-				function()
-					require("dap").run_last()
-				end,
-				desc = "Run Last",
 			},
 			{
 				"<leader>do",
@@ -94,32 +45,11 @@ return {
 				desc = "Pause",
 			},
 			{
-				"<leader>dr",
-				function()
-					require("dap").repl.toggle()
-				end,
-				desc = "Toggle REPL",
-			},
-			{
-				"<leader>ds",
-				function()
-					require("dap").session()
-				end,
-				desc = "Session",
-			},
-			{
 				"<leader>dt",
 				function()
 					require("dap").terminate()
 				end,
 				desc = "Terminate",
-			},
-			{
-				"<leader>dw",
-				function()
-					require("dap.ui.widgets").hover()
-				end,
-				desc = "Widgets",
 			},
 			{
 				"<Leader>du",
